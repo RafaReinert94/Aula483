d115a3cf50d9582659f5d25d85f90c6ec4c2fb78
Rafael Reinert <rafareinert94@hotmail.com>
2019-12-02 21:55:30 -0300

    Exercicios Carros

---
+++ CarrosLista/CarrosLista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrosLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> carrosLoja = new List<string>
+            {
+                "Corsa",
+                "Celta",
+                "207",
+                "Onix",
+                "Gol",
+                "Golf",
+            };
+
+            carrosLoja.ForEach(x => Console.WriteLine(x));
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
