d81a4ab1e551f4ae3cc4cccbf198921a5767991d
Rafael Reinert <rafareinert94@hotmail.com>
2019-12-03 22:06:46 -0300

    Metodos Feitos

--- NomeDeFilmes/NomeDeFilmes/Program.cs
+++ NomeDeFilmes/NomeDeFilmes/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NomeDeFilmes
 {
@@ -11,49 +8,67 @@ namespace NomeDeFilmes
         static List<string> NomeDeFilmes = new List<string>();
         static void Main(string[] args)
         {
-
-            Console.WriteLine("--------------------");
-            Console.WriteLine("Top Movies");
-            Console.WriteLine("--------------------");
-
+            MensagemInicial();
 
             var opcaoSelecionada = "0";
 
             while (opcaoSelecionada != "5")
             {
-
-                Console.WriteLine("Digite o numero da opção desejada");
-                Console.WriteLine("1 - Adicionar Filmes");
-                Console.WriteLine("2 - Listar Filmes");
-                Console.WriteLine("3 - Editar os Filmes");
-                Console.WriteLine("4 - Excluir um Filme");
-                Console.WriteLine("5 - Sair do Sistema");
-
-                opcaoSelecionada = Console.ReadLine();
+                opcaoSelecionada = MostrarMenu();
 
                 switch (opcaoSelecionada)
-            {
-                case "1":
-                    Console.WriteLine("Digite o nome do Melhor Filme");
-                    var filmeDigitado = Console.ReadLine();
-                    NomeDeFilmes.Add(filmeDigitado);
-                    Console.WriteLine("Filme adicionado com sucesso");
-                    Console.ReadKey(true);
-                    break;
-                case "2":
-                    Console.WriteLine("Listagem do melhores filmes");
-                    NomeDeFilmes.ForEach(x => Console.WriteLine(x));
-                    Console.WriteLine("Lista carregada com sucesso");
-                    Console.ReadKey(true);
-                    break;
-                default:
-                    Console.WriteLine("Opção Inválida");
-                    break;
-            }
+                {
+                    case "1":
+                        DigitarFilme();
+                        break;
+                    case "2":
+                        ListarFilmes();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
                 Console.Clear();
             }
 
 
+        }
+
+        private static string MostrarMenu()
+        {
+            Console.WriteLine("Digite o numero da opção desejada");
+            Console.WriteLine("1 - Adicionar Filmes");
+            Console.WriteLine("2 - Listar Filmes");
+            Console.WriteLine("3 - Editar os Filmes");
+            Console.WriteLine("4 - Excluir um Filme");
+            Console.WriteLine("5 - Sair do Sistema");
+
+            return Console.ReadLine(); ;
+        }
+
+        private static void MensagemInicial()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Top Movies");
+            Console.WriteLine("--------------------");
+        }
+
+        private static void ListarFilmes()
+        {
+            Console.WriteLine("Listagem do melhores filmes");
+            NomeDeFilmes.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("Lista carregada com sucesso");
+            Console.ReadKey(true);
+        }
+
+        private static void DigitarFilme()
+        {
+            Console.WriteLine("Digite o nome do Melhor Filme");
+            var filmeDigitado = Console.ReadLine();
+            NomeDeFilmes.Add(filmeDigitado);
+            Console.WriteLine("Filme adicionado com sucesso");
+            Console.ReadKey(true);
         }
     }
 }
--- RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
+++ RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
@@ -13,47 +13,21 @@ namespace RegistroCivilDeNomes
         static List<string> ListaDeNomes = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("*************************");
-            Console.WriteLine("***Sistema de Registro***");
-            Console.WriteLine("*************************");
-            Console.WriteLine("");
-            Console.WriteLine("");
-
-
+            MensagemInicial();
 
             var menuEscolhido = "0";
 
             while (menuEscolhido != "5")
             {
-                Console.WriteLine("Digite a opção desejada");
-                Console.WriteLine("1 - Registrar Nome");
-                Console.WriteLine("2 - Listar Nomes");
-                Console.WriteLine("3 - Editar Um Nome");
-                Console.WriteLine("4 - Excluir Nomes");
-                Console.WriteLine("5 - Sair do Sistema");
-
-
-                menuEscolhido = Console.ReadLine();
+                menuEscolhido = MostrarMenu();
 
                 switch (menuEscolhido)
                 {
                     case "1":
-                        Console.WriteLine("Registro de Nome Selecionado");
-                        Console.WriteLine("Informe um Nome:");
-                        var nomeInformado = Console.ReadLine();
-
-                        ListaDeNomes.Add(nomeInformado);
-
-                        Console.WriteLine("Nome Informado com sucesso");
-                        Console.ReadKey(true);
+                        RegistrarNome();
                         break;
                     case "2":
-                        Console.WriteLine("Listagem de Nome Selecionado");
-
-                        ListaDeNomes.ForEach(x => Console.WriteLine($"Nome:^{x}"));
-
-                        Console.WriteLine("Listagem de nomes finalizada.");
-                        Console.ReadKey(true);
+                        MostrarLista();
                         break;
                     default:
                         Console.WriteLine("Menu Inválido");
@@ -64,11 +38,50 @@ namespace RegistroCivilDeNomes
                 Console.Clear();
             }
 
-
-
-
             Console.ReadKey(true);
-
         }
+
+        private static void MensagemInicial()
+        {
+            Console.WriteLine("*************************");
+            Console.WriteLine("***Sistema de Registro***");
+            Console.WriteLine("*************************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        static void RegistrarNome()
+        {
+            Console.WriteLine("Registro de Nome Selecionado");
+            Console.WriteLine("Informe um Nome:");
+            var nomeInformado = Console.ReadLine();
+
+            ListaDeNomes.Add(nomeInformado);
+
+            Console.WriteLine("Nome Informado com sucesso");
+            Console.ReadKey(true);
+        }
+
+        static void MostrarLista()
+        {
+            Console.WriteLine("Listagem de Nome Selecionado");
+
+            ListaDeNomes.ForEach(x => Console.WriteLine($"Nome:^{x}"));
+
+            Console.WriteLine("Listagem de nomes finalizada.");
+            Console.ReadKey(true);
+        }
+
+        static string MostrarMenu()
+        {
+            Console.WriteLine("Digite a opção desejada");
+            Console.WriteLine("1 - Registrar Nome");
+            Console.WriteLine("2 - Listar Nomes");
+            Console.WriteLine("3 - Editar Um Nome");
+            Console.WriteLine("4 - Excluir Nomes");
+            Console.WriteLine("5 - Sair do Sistema");
+            return Console.ReadLine();
+        }
+
     }
 }
