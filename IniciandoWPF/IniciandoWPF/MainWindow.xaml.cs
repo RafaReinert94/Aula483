06bf2e632b1647fca9fd07cbe2411b1011662978
Rafael Reinert <rafareinert94@hotmail.com>
2019-12-05 22:17:39 -0300

    AulaDeClasse

---
+++ IniciandoWPF/IniciandoWPF/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IniciandoWPF
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGrid.ItemsSource = ListaDeNomes;
+        }
+
+        List<string> ListaDeNomes = new List<string>
+        {
+            "Chael",
+            "Rocneth",
+            "Liamcyn",
+            "Dicjohn",
+            "Dahe",
+            "Chellren",
+            "Kimni",
+            "Hadod",
+            "Garnyahildi",
+            "Reginisen",
+            "Danmax",
+            "Leysig",
+            "Hesfastnath",
+            "Jenba",
+            "Marmaand",
+            "Gorma",
+            "Helm",
+            "Isumlin",
+            "Jenann",
+            "Doegar"
+        };
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+
+
+            switch (cbxFiltro.SelectedIndex)
+            {
+                case 0:
+                    OrdenarLista();
+                    break;
+                case 1:
+                    FiltraPorLetras(tbxFiltro.Text);
+                    break;
+                case 2:
+                    OrdenarMaiorParaMenor();
+                    break;
+
+            }
+             //MessageBox.Show(tbxFiltro.Text);
+             //MessageBox.Show("Olá WFP");
+        }
+
+        private void OrdenarLista()
+        {
+            dataGrid.ItemsSource = ListaDeNomes.OrderBy(x => x);
+        }
+
+        private void FiltraPorLetras(string parametro)
+        {
+            dataGrid.ItemsSource = ListaDeNomes.Where(x => x.Contains(parametro));
+        }
+
+        private void OrdenarMaiorParaMenor()
+        {
+            dataGrid.ItemsSource = ListaDeNomes.OrderByDescending(x => x.Length);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MostrarLista();
+        }
+
+
+        List<string> ListaDeFilme = new List<string>
+        {
+            "Avengers",
+            "Transformes",
+            "7 seens",
+            "Hotel transilvania",
+            "Incriveis"
+        };
+
+        private void FiltrarPeloTexto()
+        {
+            dataGrid.ItemsSource = ListaDeFilme.Where(x => x.Contains(tbxText.Text));
+        }
+
+
+        private void BtnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+            switch (cbxFiltro.SelectedIndex)
+            {
+                
+                case 0:
+                    dataGrid.ItemsSource = ListaDeFilme.OrderBy(x => x);
+                    break;
+                case 1:
+                    FiltrarPeloTexto();
+                    break;
+            }
+            
+        }
+
+        private void BtnAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            AdicionarFilme();
+            dataGrid.ItemsSource = null;
+            MostrarLista();
+
+        }
+
+        private void AdicionarFilme()
+        {
+            ListaDeFilme.Add(tbxText.Text);
+        }
+
+        private void MostrarLista()
+        {
+            dataGrid.ItemsSource = ListaDeFilme;
+        }
+
+        private void BtnExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            ExcluirFilme(tbxText.Text);
+            MostrarLista();
+        }
+
+        private void ExcluirFilme(string delete)
+        {
+            ListaDeFilme.Remove(delete);
+        }
+    }
+}
